0653d64de73d5c1ff8acfa7be4285450e37d9bb5
r s <rs64@wsl31.informatik.uni-ulm.de>
2023-06-23 17:11:31 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.platformer_game {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.platformer_game to javafx.fxml;
+    exports com.example.platformer_game;
+}
